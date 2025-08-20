9ea1402bc2aacb4b5f1236871c659b5b847eb2a3
Matt Frazier <frazier3388@yahoo.com>
2025-08-20 13:13:41 +0900

    Implement a function to reverse a string

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,17 @@
-const reverseString = function() {
+/* PEDAC
+PROBLEM: GIVEN A STRING ARGUMENT, REVERSE IT
+EXAMPLES: EXPECT "HELLO" TO EQUAL "OLLEH"
+DATA STRUCTURES: STRINGS AND ARRAYS
+ALGORITHM: 
+1. GIVEN A STRING ARGUMENT, SPLIT IT INTO AN ARRAY OF CHARS
+2. REVERSE THE ARRAY
+3. JOIN THE REVERSED ARRAY AND RETURN THE RESULT
+*/
 
+const reverseString = function (str) {
+  const strArr = str.split("")
+  const reversedArr = strArr.reverse();
+  return reversedArr.join("");
 };
 
 // Do not edit below this line
