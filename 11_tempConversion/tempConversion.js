1d708e02ced54ab78c81fceb8b34b72daddf971c
Matt Frazier <frazier3388@yahoo.com>
2025-08-21 08:50:27 +0900

    Complete algorithm for temperature conversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+/* PEDAC
+PROBLEM: GIVEN A NUMBER ARGUMENT IN FAHRENHEIT OR CELSIUS CONVERT THE TEMP TO THE OTHER MEASUREMENT -> TWO SEPARATE FUNCTIONS
+EXAMPLES: convertToCelsius(32) // fahrenheit to celsius, should return 0
+          convertToFahrenheit(0) // celsius to fahrenheit, should return 32 
+DATA STRUCTURES: NUMBERS
+ALGORITHM:
+1. °F = (°C × 9/5) + 32
+2. °C = (°F − 32) × 5/9
+*/
+
+const convertToCelsius = function (fahrenheit) {
+  return Number(((fahrenheit - 32) * 5 / 9).toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  return Number(((celsius * (9 / 5)) + 32).toFixed(1))
 };
 
 // Do not edit below this line
